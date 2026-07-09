aa9203b16c461a7ab7005643f2aa910261b62842
mdmustakim9752-byte <mustakim9752@gmail.com>
2026-07-09 11:27:04 -0400

    Variable and data types basic practical done

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
+
 #include <stdio.h>
 
 int main() {
-	printf("Mission Cloud Architect Started at 2 AM!\n");
+	// 1. Vairable banao aur data store karo 
+	int age = 20;
+	char mylatter = 'M';
+
+	// 2. RAM se data nikal kar screen par print karo 
+	// (% integer ke liye hota hai, aur %c character ke liye)
+	printf("Mustakim ki age hai: %d saal\n", age);
+	printf("Name ka pehla akshar hai: %c\n", mylatter);
+
 	return 0;
 }
