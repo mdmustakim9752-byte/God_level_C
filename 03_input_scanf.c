df49a1722873b933bfeb11723a591868ad06b984
mdmustakim9752-byte <mustakim9752@gmail.com>
2026-07-19 16:39:01 -0400

    Added interactive user input practical using scanf

---
+++ 03_input_scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+	int userAge;
+
+	// 1. User ko screen par ek message dikhao
+	printf ("Mustakim, apni sahi age type karo aur Enter dabao: ");
+
+	// 2. keyboard se input lo aur use 'userAge' ke address (&) Par store karo 
+	scanf ("%d", &userAge);
+
+	//.3. jo user ne type kiya, use wapas print karke confirm karo
+	printf("Wah! tumne enter kiya ki tumhari age %d saal hai!\n", userAge);
+
+	return 0;
+}
