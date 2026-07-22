a1fb1fcf60a94aeee89cd764585d191730a7f2d5
mdmustakim9752-byte <mustakim9752@gmail.com>
2026-07-22 13:37:56 -0400

    Added Module 3 Control Statements - if-else condition practical

---
+++ 04_if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+	int main() {
+		int age;
+
+	// 1. user se age input maango
+	printf("Mustakim, apni age enter karo: ");
+	scanf("%d", &age);
+
+	// 2. Condition check karo 
+	if (age >= 18) {
+		printf("Sahi hai boss! tum 18+ ho, Vote de sakte ho! \n");
+	}else{
+		printf("Abhi tum chote ho! Vote nahi de sakte. \n");
+	}
+	
+	return 0;
+}
