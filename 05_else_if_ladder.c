432b3ba5a6024f723d40003766ce815891b0f202
Mohammad Mustakim <mdmustakim9752@gmail.com>
2026-07-24 01:09:45 +0530

    Create 05_else_if_ladder.c

---
+++ 05_else_if_ladder.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+        int marks;
+
+        printf("Enter your marks (0-100):");
+        scanf("%d", &marks);
+
+        if (marks >= 90) {
+                printf("Result: Grade A (Outstanding!)\n");
+        }
+        else if (marks >= 75){
+                printf("Result: Grade B (Good job!)\n");
+        }
+        else if (marks >= 50) {
+                printf("Result: Grade C(Satisfaing)\n");
+        }
+        else {
+                printf("Result: Failed (Needs Improvement)\n");
+        }
+        return 0;
