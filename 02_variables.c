5d6d4ab53d13420c9a35bc1bea27d67fb8684c62
Mohammad Mustakim <mdmustakim9752@gmail.com>
2026-08-13 23:59:24 +0530

    Update 02_variables.c

--- 02_variables.c
+++ 02_variables.c
@@ -10,7 +10,6 @@ int main() {
 	// (% integer ke liye hota hai, aur %c character ke liye)
 	printf("Mustakim ki age hai: %d saal\n", age);
 	printf("Name ka pehla akshar hai: %c\n", mylatter);
-
 	
 	return 0;
 }
